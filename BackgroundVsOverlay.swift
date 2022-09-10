774b489b21ed8b89a545df3c76a98917b440c5d8
Mikhail Melikhov <91681682+taosade@users.noreply.github.com>
2022-09-10 13:54:17 +0300

    Create BackgroundVsOverlay.swift

---
+++ BackgroundVsOverlay.swift
@@ -0,0 +1,33 @@
+// Playing around with view's backgrounds and overlays.
+
+import SwiftUI
+
+struct BackgroundVsOverlay: View
+{
+    var body: some View
+    {
+        Circle()
+            .fill(Color.red)
+            .frame(width: 200, height: 200)
+            .background(
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(width: 250, height: 250),
+                alignment: .topLeading
+            )
+            .overlay(
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: 100, height: 100),
+                alignment: .bottomTrailing
+            )
+    }
+}
+
+struct Frame_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        BackgroundVsOverlay()
+    }
+}
