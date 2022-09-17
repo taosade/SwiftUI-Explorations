d920602833bb8eda2cfd3c34e14cce8ed166d840
Mikhail Melikhov <91681682+taosade@users.noreply.github.com>
2022-09-17 12:26:30 +0300

    Create ScrollViews.swift

---
+++ ScrollViews.swift
@@ -0,0 +1,50 @@
+// Implementation of vertical and horizontal scrollviews
+
+import SwiftUI
+
+struct ScrollViews: View
+{
+    let data: [String] = ["Troll", "Face", "JPG"]
+
+    var body: some View
+    {
+        ScrollView(.vertical, showsIndicators: false)
+        {
+            VStack
+            {
+                ForEach(1..<10)
+                {
+                    indexV in
+
+                    ScrollView(.horizontal, showsIndicators: false)
+                    {
+                        HStack
+                        {
+                            ForEach(1..<10)
+                            {
+                                indexH in
+
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.yellow)
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                    .overlay(Text("\(indexV) : \(indexH)"))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViews_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        HStack
+        {
+            ScrollViews()
+        }
+    }
+}
