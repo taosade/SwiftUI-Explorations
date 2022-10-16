8f27ea479fe6637a7d42a41db89b7950c3ea4178
Mikhail Melikhov <91681682+taosade@users.noreply.github.com>
2022-10-16 16:49:03 +0300

    Create Badges.swift

---
+++ Badges.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct BadgesView: View
+{
+    var body: some View
+    {
+        TabView
+        {
+            List
+            {
+                ForEach([1, 2, 3], id: \.self)
+                {
+                    item in Text("Item #\(item)").badge(item == 2 ? "New!" : nil)
+                }
+            }.tabItem { Label("List", systemImage: "list.bullet") }
+
+            Color.red.tabItem
+            {
+                Image(systemName: "rectangle.fill"); Text("Tab Item")
+            }
+            .tabItem { Label("Tab Item", systemImage: "rectangle.fill") }
+            .badge(5)
+        }
+    }
+}
+
+struct Preview: PreviewProvider
+{
+    static var previews: some View
+    {
+        BadgesViewView()
+    }
+}
