2a14ecf06f0dcebb20fe92e8a05e71bb277a1317
Mikhail Melikhov <91681682+taosade@users.noreply.github.com>
2022-10-16 16:29:53 +0300

    Create SwipeActions.swift

---
+++ SwipeActions.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct SwipeActionsView: View
+{
+    var body: some View
+    {
+        NavigationView
+        {
+            List
+            {
+                ForEach([1, 2, 3, 4, 5], id: \.self)
+                {
+                    item in Text("Item #\(item)")
+                        .swipeActions(edge: .trailing, allowsFullSwipe: false)
+                        {
+                            Button
+                            {
+                                // Delete action handler
+                            }
+                            label: { Label("Delete", systemImage: "trash.fill") }.tint(.red)
+
+                            Button("Archive")
+                            {
+                                // Archive action handler
+                            }
+                            .tint(.black)
+                        }
+                        .swipeActions(edge: .leading, allowsFullSwipe: true)
+                        {
+                            Button
+                            {
+                                // Check action handler
+                            }
+                            label: { Label("Check", systemImage: "checkmark") }.tint(.green)
+                        }
+                }
+            }.navigationTitle("List Items")
+        }
+    }
+}
+
+struct Preview: PreviewProvider
+{
+    static var previews: some View
+    {
+        SwipeActionsView()
+    }
+}
